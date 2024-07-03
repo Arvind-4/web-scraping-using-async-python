57194e1f0980e946353c8c9e6a15b11b4766ec1e
Arvind <pingarvindforquestions@gmail.com>
2024-07-03 09:27:27 +0000

    feat: linted and formatted files

--- src/main.py
+++ src/main.py
@@ -1,41 +1,46 @@
-from aiohttp import ClientSession
 import asyncio
 import pathlib
 
-async def fetch(url, session, year):
-	async with session.get(url) as response:
-		html_body = await response.read()
-		return {'Body': html_body, 'Year': year}
+from aiohttp import ClientSession
+
+OUTPUT_FOLDER_NAME = "result"
 
 
-async def fetch_with_sem(url, session, year, sem):
-	async with sem:
-		return await fetch(url, session, year)
+async def fetch(url: str, session: ClientSession, year: int) -> dict:
+    async with session.get(url) as response:
+        html_body = await response.read()
+        return {"Body": html_body, "Year": year}
 
-async def main(start_year=2020, years_ago=40):	
-	tasks = []
-	sem = asyncio.Semaphore(10)
-	async with ClientSession() as session:
-		for i in range(0, years_ago):
-			year = start_year - i
-			url = f'https://www.boxofficemojo.com/year/{year}/'
-			print(f'Year: {year}')
-			tasks.append(
-				asyncio.create_task(
-					fetch_with_sem(url, session, year, sem)))
-		page_content = await  asyncio.gather(*tasks)
-		return page_content
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+async def fetch_with_sem(
+    url: str, session: ClientSession, year: int, sem: asyncio.Semaphore
+) -> fetch:
+    async with sem:
+        return await fetch(url, session, year)
+
+
+async def main(start_year: int = 2021, years_ago: int = 5) -> list:
+    tasks = []
+    sem = asyncio.Semaphore(10)
+    async with ClientSession() as session:
+        for i in range(years_ago):
+            year = start_year - i
+            url = f"https://www.boxofficemojo.com/year/{year}/"
+            print(f"Year: {year}")
+            tasks.append(
+                asyncio.create_task(fetch_with_sem(url=url, session=session, year=year, sem=sem))
+            )
+        return await asyncio.gather(*tasks)
+
+
 results = asyncio.run(main())
 
-file_name = 'Results'
-output_dir = pathlib.Path().resolve() / file_name
+output_dir = pathlib.Path().resolve() / OUTPUT_FOLDER_NAME
 output_dir.mkdir(parents=True, exist_ok=True)
 
 for result in results:
-	current_year = result.get('Year')
-	html_data = result.get('Body')
+    current_year = result.get("Year")
+    html_data = result.get("Body")
 
-	output_file = output_dir / f'{current_year}.html' 
-	output_file.write_text(html_data.decode())
+    output_file = output_dir / f"{current_year}.html"
+    output_file.write_text(html_data.decode())
